49775cd0d034b1c891c0284d09c340b6cf8e82cb
Mano Cardozo <manasses.cardozo@gmail.com>
2020-09-20 07:30:18 +1200

    Add SDK and sample projects

---
+++ Tweetbook/Tweetbook.API.SDK.Sample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Tweetbook.Contracts.V1.Requests;
+using Refit;
+
+namespace Tweetbook.API.SDK.Sample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var tweetbookApi = RestService.For<ITweetbookApi>("https://localhost:5001");
+
+            var postCreated = await tweetbookApi.CreateAsync(new CreatePostRequest
+            {
+                Name = "Created from SDK"
+            });
+
+            var post = await tweetbookApi.GetAsync(postCreated.Content.Id);
+
+            var postUpdated = await tweetbookApi.UpdateAsync(post.Content.Id, new UpdatePostRequest
+            {
+                Name = "Updated from SDK"
+            });
+
+            var allPosts = await tweetbookApi.GetAllAsync();
+
+            await tweetbookApi.DeleteAsync(post.Content.Id);
+        }
+    }
+}
